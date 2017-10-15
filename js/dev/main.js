c2210390a0bcd9ed439f1d0876e2981daefac910
kirillvladykin <kirillmybox@rambler.ru>
2017-10-15 12:29:43 +0300

    -t

--- js/dev/main.js
+++ js/dev/main.js
@@ -27,6 +27,8 @@ var x_sec = 0
   , ctx = null
   , timercanvas = null
   , timerctx = null
+  , countcanvs = null
+  , countctx = null
 
   , current_Time = null
   , currenthours = null
@@ -37,6 +39,9 @@ var x_sec = 0
   , alarmHours2 = 0
   , alarmMinutes1 = 0
   , alarmMinutes2 = 0
+
+  , isClockRun = false
+  , isCountdownRun = false
 
   , sound = 0
   , audio = null
@@ -51,6 +56,9 @@ var x_sec = 0
   , countSeconds2 = 0
   , countdownIntervalId = 0
   , parserUserAgent = null
+
+  , countFillStyle = "black"
+  , countFlag = 0
 
   , melodies = {
         mp3: [
@@ -113,11 +121,83 @@ function initTime() {
     currentseconds = current_Time.getSeconds();
 
     setInterval(function() {
-        current_Time = new Date,
-        currenthours = current_Time.getHours(),
-        currentminutes = current_Time.getMinutes(),
-        currentseconds = current_Time.getSeconds()
-    }, 1e3);    
+        current_Time = new Date;
+        currenthours = current_Time.getHours();
+        currentminutes = current_Time.getMinutes();
+        currentseconds = current_Time.getSeconds();
+
+        if (isClockRun) {
+            stepClock();
+        }
+
+        if (isCountdownRun) {
+            stepCountdown();
+        }
+    }, 1000);    
+}
+function initClock() {
+    mycanvas = document.getElementById("clockcanvas")
+    ctx = mycanvas.getContext("2d");
+
+    ctx.beginPath(),
+    ctx.fillStyle = "black",
+    ctx.arc(292, 240, 200, 0, 2 * Math.PI, !0),
+    ctx.fill(),
+    ctx.closePath(),
+    ctx.beginPath(),
+    ctx.fillStyle = "white",
+    ctx.arc(292, 240, 195, 0, 2 * Math.PI, !0),
+    ctx.fill(),
+    ctx.closePath(),
+    ctx.beginPath(),
+    ctx.fillStyle = "black",
+    ctx.arc(292, 240, 6, 0, 2 * Math.PI, !0),
+    ctx.fill(),
+    ctx.closePath(),
+    ctx.beginPath(),
+    ctx.fillStyle = "black",
+    ctx.font = "bold 32px Arial, arial",
+    ctx.fillText("1", 370, 98, 200),
+    ctx.fillText("2", 436, 163, 200),
+    ctx.fillText("3", 463, 250, 200),
+    ctx.fillText("4", 438, 340, 200),
+    ctx.fillText("5", 373, 405, 200),
+    ctx.fillText("6", 284, 428, 200),
+    ctx.fillText("7", 196, 405, 200),
+    ctx.fillText("8", 128, 335, 200),
+    ctx.fillText("9", 105, 250, 200),
+    ctx.fillText("10", 120, 166, 200),
+    ctx.fillText("11", 185, 105, 200),
+    ctx.fillText("12", 273, 77, 200);
+
+    $("#saat").tzineClock();
+
+    angle_hour = (currenthours % 12 * 10 + Math.floor(currentminutes / 6)) * (Math.PI / 60),
+    angle_min = currentminutes * Math.PI / 30,
+    angle_sec = currentseconds * Math.PI / 30,
+    min_interval = 59 - currentseconds,
+    hour_interval = 359 - currentminutes % 6 * 60 - currentseconds,
+    
+    clockHours();
+    clockMinutes();
+    clockSeconds();
+
+    isClockRun = true;
+}
+function stepClock() {
+    if (hour_interval == 0) {
+        clockHours();
+        hour_interval = 360;
+    }
+    hour_interval--;
+
+    if (min_interval == 0) {
+        clockMinutes();
+        min_interval = 60;
+    }
+    min_interval--;
+
+    clockSeconds();
 }
 function clockHours() {
     erase(x_hour_er, y_hour_er),
@@ -128,11 +208,6 @@ function clockHours() {
     angle_hour += Math.PI / 60,
     drawHours(x_hour, y_hour)
 }
-function intervalClockHours() {
-    0 == hour_interval && (clockHours(),
-    hour_interval = 360),
-    hour_interval--
-}
 function clockMinutes() {
     erase(x_min_er, y_min_er),
     x_min = 292 + 150 * Math.sin(angle_min),
@@ -142,11 +217,6 @@ function clockMinutes() {
     angle_min += Math.PI / 30,
     drawHours(x_hour, y_hour),
     drawMinutes(x_min, y_min)
-}
-function intervalClockMinutes() {
-    0 == min_interval && (clockMinutes(),
-    min_interval = 60),
-    min_interval--
 }
 function clockSeconds() {
     erase(x_sec_er, y_sec_er),
@@ -200,55 +270,7 @@ function drawSeconds(e, t) {
     ctx.stroke(),
     ctx.closePath()
 }
-function initClock() {
-    mycanvas = document.getElementById("clockcanvas")
-    ctx = mycanvas.getContext("2d");
 
-    ctx.beginPath(),
-    ctx.fillStyle = "black",
-    ctx.arc(292, 240, 200, 0, 2 * Math.PI, !0),
-    ctx.fill(),
-    ctx.closePath(),
-    ctx.beginPath(),
-    ctx.fillStyle = "white",
-    ctx.arc(292, 240, 195, 0, 2 * Math.PI, !0),
-    ctx.fill(),
-    ctx.closePath(),
-    ctx.beginPath(),
-    ctx.fillStyle = "black",
-    ctx.arc(292, 240, 6, 0, 2 * Math.PI, !0),
-    ctx.fill(),
-    ctx.closePath(),
-    ctx.beginPath(),
-    ctx.fillStyle = "black",
-    ctx.font = "bold 32px Arial, arial",
-    ctx.fillText("1", 370, 98, 200),
-    ctx.fillText("2", 436, 163, 200),
-    ctx.fillText("3", 463, 250, 200),
-    ctx.fillText("4", 438, 340, 200),
-    ctx.fillText("5", 373, 405, 200),
-    ctx.fillText("6", 284, 428, 200),
-    ctx.fillText("7", 196, 405, 200),
-    ctx.fillText("8", 128, 335, 200),
-    ctx.fillText("9", 105, 250, 200),
-    ctx.fillText("10", 120, 166, 200),
-    ctx.fillText("11", 185, 105, 200),
-    ctx.fillText("12", 273, 77, 200);
-
-    $("#saat").tzineClock();
-
-    angle_hour = (currenthours % 12 * 10 + Math.floor(currentminutes / 6)) * (Math.PI / 60),
-    angle_min = currentminutes * Math.PI / 30,
-    angle_sec = currentseconds * Math.PI / 30,
-    min_interval = 59 - currentseconds,
-    hour_interval = 359 - currentminutes % 6 * 60 - currentseconds,
-    clockHours(),
-    setInterval(intervalClockHours, 1e3),
-    clockMinutes(),
-    setInterval(intervalClockMinutes, 1e3),
-    clockSeconds(),
-    setInterval(clockSeconds, 1e3);
-}
 
 /*********/
 /* ALARM */
@@ -312,6 +334,42 @@ function decreaseAlarmMinutes() {
     alarmMinutes2 = 9),
     drawAlarm()
 }
+
+/*************/
+/* COUNTDOWN */
+/************/
+function drawRemaining(flag) {
+    if (drawCountdown) {
+        
+        countcanvas = document.getElementById("countcanvas");
+        countctx = countcanvas.getContext("2d");
+        countctx.fillStyle = "black",
+        countctx.strokeStyle = "black",
+        countctx.strokeRect(0, 0, 305, 60),
+        countctx.fillRect(0, 0, 305, 60),
+        countctx.fillStyle = "white",
+        countctx.strokeStyle = "white",
+        countctx.strokeRect(5, 5, 295, 50),
+        countctx.fillRect(5, 5, 295, 50);
+        
+        setCountdownVariables(flag);
+        
+        countctx.beginPath(),
+        countctx.fillStyle = countFillStyle,
+        countctx.font = "bold 48px Tahoma, tahoma",
+        countctx.fillText(countHours1, 17, 48, 200),
+        countctx.fillText(countHours2, 52, 48, 200),
+        countctx.fillText(":", 92, 43, 200),
+        countctx.fillText(countMinutes1, 122, 48, 200),
+        countctx.fillText(countMinutes2, 157, 48, 200),
+        countctx.fillText(":", 197, 43, 200),
+        countctx.fillText(countSeconds1, 227, 48, 200),
+        countctx.fillText(countSeconds2, 262, 48, 200),
+        countctx.closePath(),
+
+        isCountdownRun = true;
+    }
+}
 function eraseCountdownVars() {
     countHours1 = 0,
     countHours2 = 0,
@@ -320,111 +378,96 @@ function eraseCountdownVars() {
     countSeconds1 = 0,
     countSeconds2 = 0
 }
-function drawRemaining(e) {
-    if (drawCountdown) {
-        var t = document.getElementById("countcanvas").getContext("2d");
-        t.fillStyle = "black",
-        t.strokeStyle = "black",
-        t.strokeRect(0, 0, 305, 60),
-        t.fillRect(0, 0, 305, 60),
-        t.fillStyle = "white",
-        t.strokeStyle = "white",
-        t.strokeRect(5, 5, 295, 50),
-        t.fillRect(5, 5, 295, 50);
-        var n = "black"
-          , o = 0;
-        getCountDown(e),
-        t.beginPath(),
-        t.fillStyle = n,
-        t.font = "bold 48px Tahoma, tahoma",
-        t.fillText(countHours1, 17, 48, 200),
-        t.fillText(countHours2, 52, 48, 200),
-        t.fillText(":", 92, 43, 200),
-        t.fillText(countMinutes1, 122, 48, 200),
-        t.fillText(countMinutes2, 157, 48, 200),
-        t.fillText(":", 197, 43, 200),
-        t.fillText(countSeconds1, 227, 48, 200),
-        t.fillText(countSeconds2, 262, 48, 200),
-        t.closePath(),
-        countdownIntervalId = setInterval(function() {
-            0 != countSeconds1 || 0 != countSeconds2 ? countSeconds2 > 0 ? countSeconds2-- : (countSeconds1--,
-            countSeconds2 = 9) : 0 != countMinutes1 || 0 != countMinutes2 ? (countMinutes2 > 0 ? countMinutes2-- : (countMinutes1--,
-            countMinutes2 = 9),
-            countSeconds1 = 5,
-            countSeconds2 = 9) : 0 == countHours1 && 0 == countHours2 || (countHours2 > 0 ? countHours2-- : (countHours1--,
-            countHours2 = 9),
-            countMinutes2 > 0 ? countMinutes2-- : (countMinutes1--,
-            countMinutes2 = 9),
-            countMinutes1 = 5,
-            countMinutes2 = 9,
-            countSeconds1 = 5,
-            countSeconds2 = 9),
-            0 == countHours1 && 0 == countHours2 && 0 == countMinutes1 && 0 == countMinutes2 && 0 == countSeconds1 && 0 == countSeconds2 && (n = "black" == n ? "red" : "black",
-            0 == o && (enableSnooze(),
-            enableSound(),
-            document.getElementById("start_stop").innerHTML = "Проснулся",
-            o = 1)),
-            t.fillStyle = "black",
-            t.strokeStyle = "black",
-            t.strokeRect(0, 0, 305, 60),
-            t.fillRect(0, 0, 305, 60),
-            t.fillStyle = "white",
-            t.strokeStyle = "white",
-            t.strokeRect(5, 5, 295, 50),
-            t.fillRect(5, 5, 295, 50),
-            t.beginPath(),
-            t.fillStyle = n,
-            t.font = "bold 48px Tahoma, tahoma",
-            t.fillText(countHours1, 17, 48, 200),
-            t.fillText(countHours2, 52, 48, 200),
-            t.fillText(":", 92, 43, 200),
-            t.fillText(countMinutes1, 122, 48, 200),
-            t.fillText(countMinutes2, 157, 48, 200),
-            t.fillText(":", 197, 43, 200),
-            t.fillText(countSeconds1, 227, 48, 200),
-            t.fillText(countSeconds2, 262, 48, 200),
-            t.closePath()
-        }, 1e3)
+function setCountdownVariables(flag) {
+    var diffHours = -1
+      , diffMin= -1
+      , diffSec = -1
+      , nextHour = false
+      , sameHour = false
+      , sameMinute = false;
+
+    if (flag) {
+        countSeconds1 = "0";
+        countSeconds2 = "0";
+        countMinutes1 = "1";
+        countMinutes2 = "0";
+        countHours1 = "0";
+        countHours2 = "0";
+    } else {
+        dh = (alarmHours1 == "0") ? parseInt(alarmHours2) - currenthours : parseInt(alarmHours1 + "" + alarmHours2) - currenthours;
+        dm = (alarmMinutes1 == "0") ? parseInt(alarmMinutes2) - currentminutes : parseInt(alarmMinutes1 + "" + alarmMinutes2) - currentminutes;
+        ds = 60 - currentseconds;
+        
+        if (dm < 0) {
+            dm += 60;
+            nextHour = true;
+        } else if (dm == 0) {
+            sameMinute = true;
+        }
+        
+        dh < 0 ? dh = 24 + dh : 0 == dh && (sameHour = true),
+        (nextHour || sameMinute) && (sameHour ? dh = 23 : dh--),
+
+        60 == ds ? ds = 0 : 0 != dm ? dm-- : 0 != ds && (dm= 59),
+        dm += "",
+        dh += "",
+        1 == (ds += "").length ? countSeconds2 = ds : (countSeconds1 = ds.charAt(0),
+        countSeconds2 = ds.charAt(1)),
+        1 == dm.length ? countMinutes2 = dm: (countMinutes1 = dm.charAt(0),
+        countMinutes2 = dm.charAt(1)),
+        1 == dh.length ? countHours2 = dh : (countHours1 = dh.charAt(0),
+        countHours2 = dh.charAt(1))
     }
 }
-function getCountDown(e) {
-    var t = -1
-      , n = -1
-      , o = -1
-      , u = !1
-      , i = !1
-      , c = !1;
-    e ? (countSeconds1 = "0",
-    countSeconds2 = "0",
-    countMinutes1 = "1",
-    countMinutes2 = "0",
-    countHours1 = "0",
-    countHours2 = "0") : (t = "0" == alarmHours1 ? parseInt(alarmHours2) - currenthours : parseInt(alarmHours1 + "" + alarmHours2) - currenthours,
-    n = "0" == alarmMinutes1 ? parseInt(alarmMinutes2) - currentminutes : parseInt(alarmMinutes1 + "" + alarmMinutes2) - currentminutes,
-    o = 60 - currentseconds,
-    n < 0 ? (n = 60 + n,
-    u = !0) : 0 == n && (c = !0),
-    t < 0 ? t = 24 + t : 0 == t && (i = !0),
-    (u || c) && (i ? t = 23 : t--),
-    60 == o ? o = 0 : 0 != n ? n-- : 0 != o && (n = 59),
-    n += "",
-    t += "",
-    1 == (o += "").length ? countSeconds2 = o : (countSeconds1 = o.charAt(0),
-    countSeconds2 = o.charAt(1)),
-    1 == n.length ? countMinutes2 = n : (countMinutes1 = n.charAt(0),
-    countMinutes2 = n.charAt(1)),
-    1 == t.length ? countHours2 = t : (countHours1 = t.charAt(0),
-    countHours2 = t.charAt(1)))
+function stepCountdown() {
+    0 != countSeconds1 || 0 != countSeconds2 ? countSeconds2 > 0 ? countSeconds2-- : (countSeconds1--,
+    countSeconds2 = 9) : 0 != countMinutes1 || 0 != countMinutes2 ? (countMinutes2 > 0 ? countMinutes2-- : (countMinutes1--,
+    countMinutes2 = 9),
+    countSeconds1 = 5,
+    countSeconds2 = 9) : 0 == countHours1 && 0 == countHours2 || (countHours2 > 0 ? countHours2-- : (countHours1--,
+    countHours2 = 9),
+    countMinutes2 > 0 ? countMinutes2-- : (countMinutes1--,
+    countMinutes2 = 9),
+    countMinutes1 = 5,
+    countMinutes2 = 9,
+    countSeconds1 = 5,
+    countSeconds2 = 9),
+    
+    0 == countHours1 && 0 == countHours2 && 0 == countMinutes1 && 0 == countMinutes2 && 0 == countSeconds1 && 0 == countSeconds2 && (countFillStyle = "black" == countFillStyle ? "red" : "black",
+    0 == countFlag && (enableSnooze(),
+    enableSound(),
+    document.getElementById("start_stop").innerHTML = "Проснулся",
+    countFlag = 1)),
+    countctx.fillStyle = "black",
+    countctx.strokeStyle = "black",
+    countctx.strokeRect(0, 0, 305, 60),
+    countctx.fillRect(0, 0, 305, 60),
+    countctx.fillStyle = "white",
+    countctx.strokeStyle = "white",
+    countctx.strokeRect(5, 5, 295, 50),
+    countctx.fillRect(5, 5, 295, 50),
+    countctx.beginPath(),
+    countctx.fillStyle = countFillStyle,
+    countctx.font = "bold 48px Tahoma, tahoma",
+    countctx.fillText(countHours1, 17, 48, 200),
+    countctx.fillText(countHours2, 52, 48, 200),
+    countctx.fillText(":", 92, 43, 200),
+    countctx.fillText(countMinutes1, 122, 48, 200),
+    countctx.fillText(countMinutes2, 157, 48, 200),
+    countctx.fillText(":", 197, 43, 200),
+    countctx.fillText(countSeconds1, 227, 48, 200),
+    countctx.fillText(countSeconds2, 262, 48, 200),
+    countctx.closePath()
 }
 function recreateRemainingTime() {
     eraseAlarm();
-    var e = new Date
-      , t = e.getMinutes()
-      , n = e.getHours();
-    alarmHours1 = Math.floor(n / 10),
-    alarmHours2 = n % 10,
-    alarmMinutes1 = Math.floor(t / 10),
-    alarmMinutes2 = t % 10,
+    var date = new Date
+      , min = date.getMinutes()
+      , hours = date.getHours();
+    alarmHours1 = Math.floor(hours / 10),
+    alarmHours2 = hours % 10,
+    alarmMinutes1 = Math.floor(min / 10),
+    alarmMinutes2 = min % 10,
     (alarmMinutes1 += 1) >= 6 && (alarmMinutes1 = 0,
     10 == (alarmHours2 += 1) && (alarmHours2 = 0,
     alarmHours1 += 1),
@@ -469,26 +512,30 @@ function chooseAudiofileCrossbrowserly(number) {
     return melodies[audioformat][number];
 }
 function startStopUyandım() {
-    "Будить" == document.getElementById("start_stop").innerHTML ? (enableSound(),
-    disableSound(),
-    document.getElementById("start_stop").innerHTML = "Проснулся",
-    disablePlusMinusButtons(),
-    $("#button_stop").stop().fadeOut(500),
-    $("#button_play").stop().fadeOut(500),
-    document.getElementById("select").disabled = "disabled",
-    document.getElementById("remaining").innerHTML = "<div class='last1'><p class='time_rem'>Осталось спать:</p></div><div class='height'><canvas align='center' width='307' height='70' id='countcanvas'>корректно работает в Firefox и Chrome</canvas></div>",
-    drawCountdown = !0,
-    drawRemaining(!1)) : ($("#button_stop").stop().fadeIn(500),
-    $("#button_play").stop().fadeIn(500),
-    document.getElementById("start_stop").innerHTML = "Будить",
-    document.getElementById("remaining").innerHTML = "<div class='last1'><h3 class='alarm'></h3></div><div class='height'></height>",
-    disableSound(),
-    document.getElementById("snooze").disabled = "disabled",
-    enablePlusMinusButtons(),
-    document.getElementById("select").disabled = "",
-    clearInterval(countdownIntervalId),
-    eraseCountdownVars(),
-    drawCountdown = !1)
+    if (document.getElementById("start_stop").innerHTML === "Будить") {
+        enableSound();
+        disableSound();
+        document.getElementById("start_stop").innerHTML = "Проснулся";
+        disablePlusMinusButtons();
+        $("#button_stop").stop().fadeOut(500);
+        $("#button_play").stop().fadeOut(500);
+        document.getElementById("select").disabled = "disabled";
+        document.getElementById("remaining").innerHTML = "<div class='last1'><p class='time_rem'>Осталось спать:</p></div><div class='height'><canvas align='center' width='307' height='70' id='countcanvas'>корректно работает в Firefox и Chrome</canvas></div>";
+        drawCountdown = true;
+        drawRemaining(false);
+    } else {
+        $("#button_stop").stop().fadeIn(500);
+        $("#button_play").stop().fadeIn(500);
+        document.getElementById("start_stop").innerHTML = "Будить";
+        document.getElementById("remaining").innerHTML = "<div class='last1'><h3 class='alarm'></h3></div><div class='height'></height>";
+        disableSound();
+        document.getElementById("snooze").disabled = "disabled";
+        enablePlusMinusButtons();
+        document.getElementById("select").disabled = "";
+        isCountdownRun = false;
+        eraseCountdownVars();
+        drawCountdown = false;
+    }
 }
 function disablePlusMinusButtons() {
     document.getElementById("timer1").disabled = "disabled",
@@ -503,7 +550,7 @@ function enablePlusMinusButtons() {
     document.getElementById("timer4").disabled = ""
 }
 function initAudio(callback) {
-    // Must do audio element initialization according to fix limitations on mobiles
+    // Must ddiffSec audio element initialization according to fix limitations on mobiles
     // Solution from https://ru.stackoverflow.com/questions/635035/%D0%9E%D1%88%D0%B8%D0%B1%D0%BA%D0%B0-play-can-only-be-initiated-by-a-user-gesture-%D0%BC%D0%BE%D0%B1%D0%B8%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9-chrome
     //           and https://stackoverflow.com/questions/32424775/failed-to-execute-play-on-htmlmediaelement-api-can-only-be-initiated-by-a-u
     audio = new Audio(chooseAudiofileCrossbrowserly(3));
@@ -567,14 +614,14 @@ function enableSnooze() {
     document.getElementById("snooze").disabled = ""
 }
 function disableSnooze() {
-    document.getElementById("snooze").disabled = "disabled",
-    disableSound(),
-    document.getElementById("start_stop").innerHTML = "Проснулся",
-    clearInterval(countdownIntervalId),
-    eraseCountdownVars(),
-    drawCountdown = !0,
-    drawRemaining(!0),
-    recreateRemainingTime()
+    document.getElementById("snooze").disabled = "disabled";
+    disableSound();
+    document.getElementById("start_stop").innerHTML = "Проснулся";
+    isCountdownRun = false;
+    eraseCountdownVars();
+    drawCountdown = true;
+    drawRemaining(true);
+    recreateRemainingTime();
 }
 function checkRefresh() {
     document.getElementById("snooze").disabled = "disabled",
